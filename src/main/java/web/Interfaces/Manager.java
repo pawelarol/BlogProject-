5fa64ece7cc2d0ccabf87af8f6f8920d4339a7bb
pawelarol <123151436+pawelarol@users.noreply.github.com>
2024-04-13 16:02:08 +0200

    Lesson

--- src/main/java/web/Interfaces/Manager.java
+++
@@ -1,7 +0,0 @@
-package web.Interfaces;
-
-public abstract class Manager {
-    public abstract String addInf(String command);
-    // дописать остальные методы для поста, так как этот класс будет отвечать только за посты
-
-}
